abf39b2e9734d1844207bc22417b433dbe2ef711
shantanu-07l <shantanusapkal07@gmail.com>
2026-07-02 11:53:24 +0530

    Time: 9 ms (60.04%), Space: 47.6 MB (86.52%) - LeetHub

---
+++ Java/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas-07-02-2026-11-53-26.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int low=1;
+        int high=0;
+        for(int num:piles){
+            high=Math.max(high,num);
+        }
+        int result=high;
+        while(low<=high){
+            int speed=low + (high-low)/2;
+            if(isFound(piles,speed,h)){
+                result=speed;
+                high=speed-1;
+            }else{
+                low=speed+1;
+            }
+        }
+        return result;
+        
+    }
+    private boolean isFound(int piles[],int speed,int h){
+        long hours=0;
+        for(int num:piles){
+            hours+=(long) (num+speed-1)/speed;
+        }
+        return hours<=h;
+    }
+}
