cbaee1744a10cba1f2fe960efe733fd908ee0d0e
shantanu-07l <shantanusapkal07@gmail.com>
2026-07-03 23:44:57 +0530

    Time: 0 ms (100%), Space: 48.3 MB (11.24%) - LeetHub

---
+++ Java/Medium/0050-powx-n/0050-powx-n-07-03-2026-23-44-56.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return power(x, N);
+    }
+
+    private double power(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        double half = power(x, n / 2);
+        double ans = half * half;
+
+        if (n % 2 == 1) {
+            ans *= x;
+        }
+
+        return ans;
+    }
+}
