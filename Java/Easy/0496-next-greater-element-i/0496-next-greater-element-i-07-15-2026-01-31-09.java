7304a79a3ff4fd9badd7faff739c1050e36c39a6
shantanu-07l <shantanusapkal07@gmail.com>
2026-07-15 01:31:08 +0530

    Time: 4 ms (74.73%), Space: 45.5 MB (11.2%) - LeetHub

---
+++ Java/Easy/0496-next-greater-element-i/0496-next-greater-element-i-07-15-2026-01-31-09.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
+        Stack<Integer> st=new Stack<>();
+        //store in hash map num and its greater 
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!st.isEmpty() && nums2[i] > st.peek()){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                map.put(nums2[i],-1);
+            }else{
+                map.put(nums2[i],st.peek());
+            }
+            st.push(nums2[i]);
+        }
+        //iterate in nums1 return that elemnt next greter because quetion ask that nums1 is subset of nums2
+        int ans[]=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            ans[i]=map.get(nums1[i]);
+        }
+        return ans;
+        
+        
+    }
+}
