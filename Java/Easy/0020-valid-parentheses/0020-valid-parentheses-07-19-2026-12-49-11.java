c418ca1c7aea55a5369f160517fb18c5510e7a12
shantanu-07l <shantanusapkal07@gmail.com>
2026-07-19 12:49:09 +0530

    Time: 4 ms (38.09%), Space: 43.4 MB (41.55%) - LeetHub

---
+++ Java/Easy/0020-valid-parentheses/0020-valid-parentheses-07-19-2026-12-49-11.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+           
+           if(!st.isEmpty() && ( ch==')' && st.peek()=='(' || ch ==']' && st.peek() == '[' || ch=='}' && st.peek() =='{')){
+                    st.pop();
+           }else{
+            st.push(ch);
+           }
+           
+        }
+        if(st.isEmpty()){
+            return true;
+        }
+        return false;
+        
+    }
+}
