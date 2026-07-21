840a2cf0a31399d60c4c780ad82f76c43d7290e8
shantanu-07l <shantanusapkal07@gmail.com>
2026-07-21 15:17:01 +0530

    Time: 8 ms (97.18%), Space: 46.5 MB (83.29%) - LeetHub

---
+++ Java/Medium/0402-remove-k-digits/0402-remove-k-digits-07-21-2026-15-17-00.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        StringBuilder sb=new StringBuilder();
+
+        for(char ch:num.toCharArray()){
+            //if cuurrent is smaller that pyq then delete it becuse then make samller
+            while( k>0 && sb.length() > 0 && sb.charAt(sb.length() -1) > ch){
+                sb.deleteCharAt(sb.length() -1);
+                k--;
+
+            }
+            sb.append(ch);
+        }
+
+        sb.setLength(sb.length()-k);
+        int start=0;
+        while(start < sb.length() && sb.charAt(start)=='0'){
+            start++;
+        }
+        String ans=sb.substring(start);
+
+        return ans.length()==0 ? "0":ans;
+        
+    }
+}
