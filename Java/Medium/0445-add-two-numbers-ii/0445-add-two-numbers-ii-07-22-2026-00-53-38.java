e0700afbbdd11fd5c913e2ad64d37dcd6c078796
shantanu-07l <shantanusapkal07@gmail.com>
2026-07-22 00:53:38 +0530

    Time: 1 ms (100%), Space: 46.4 MB (82.82%) - LeetHub

---
+++ Java/Medium/0445-add-two-numbers-ii/0445-add-two-numbers-ii-07-22-2026-00-53-38.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        // Reverse both lists
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        // Add numbers
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit2 = (l2 != null) ? l2.val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            carry = sum / 10;
+
+            temp.next = new ListNode(sum % 10);
+            temp = temp.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        // Reverse the result
+        return reverse(dummy.next);
+    }
+}
