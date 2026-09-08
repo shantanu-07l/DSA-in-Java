cfe3c84f6d3618874c351290a20d5cdbc1fc0d1a
Shantanu Sapkal <shantanusapkal07@gmail.com>
2026-09-08 18:53:24 +0530

    Time: 8 ms (8.47%), Space: 42.6 MB (39.85%) - LeetHub

---
+++ Java/Easy/3870-count-commas-in-range/3870-count-commas-in-range-09-08-2026-18-53-27.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countCommas(int n) {
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if(i>=1000){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
